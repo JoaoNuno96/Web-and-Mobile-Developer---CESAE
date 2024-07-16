18cb0bd994b1a378251afb88609d85d95d6aeb5b
JoaoNuno96 <joaonuno1996@hotmail.com>
2024-07-16 20:10:39 +0100

    Web Cliente - Inserir Media Queries e Icon Hamburgo

---
+++ Desenvolvimento Web - Client Side/Página Cesae Exercicio/js/script.js
@@ -0,0 +1,12 @@
+function dropDown()
+{
+    var index = document.getElementById("myLinks");
+    if(index.style.display === "none")
+    {
+        index.style.display = "block";
+    }
+    else
+    {
+        index.style.display = "none";
+    }
+}
