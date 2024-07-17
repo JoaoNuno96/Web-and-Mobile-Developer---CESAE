b22582af6d89f38edadd0f634601d2ee4f06e040
JoaoNuno96 <joaonuno1996@hotmail.com>
2024-07-17 21:38:44 +0100

    Atualização de Exercicio Web Client

--- Desenvolvimento Web - Client Side/Bootstrap/Exercicio WebSite/script.js
+++ Desenvolvimento Web - Client Side/Bootstrap/Exercicio WebSite/script.js
@@ -1,5 +1,72 @@
-setTimeout(function(){
-    document.querySelector("butoon");
-    document.getElementById('btnStartVisit').dispatchEvent(new MouseEvent("click"));
-},2000);
 
+//Clicar botao
+//disparar acçao -> auto scroll 100px
+
+//COMEÇAR TRANSITAR PARA QUESTOES
+function comecar()
+{
+    window.scroll({
+        top: 620,
+        left: 100,
+        behavior: "smooth",
+      });
+
+}
+
+function noticias()
+{
+    window.scroll({
+        top: 1150,
+        left: 100,
+        behavior: "smooth",
+      });
+}
+
+function fofoquices()
+{
+    window.scroll({
+        top: 2650,
+        left: 100,
+        behavior: "smooth",
+      });
+}
+
+function voltar()
+{
+    window.scroll({
+        top: 0,
+        left: 100,
+        behavior: "smooth",
+      });
+}
+
+function registarFofoca()
+{
+    var texto = document.getElementById("historia").value;
+
+    if(texto == "")
+    {   
+        //div row
+        var divRow = document.getElementById("div");
+        divRow.classList.add("row");
+
+        //div col
+        var divCol = document.createElement("div");
+        divCol.classList.add("col");
+
+        //Elemento alert
+        var div = document.createElement("div");
+        div.classList.add("alert");
+        div.classList.add("alert-warning");
+        div.role = "alert";
+        div.innerHTML = "Por favor, verifique se tem algum texto na historia."
+
+
+        document.getElementById("formulario").appendChild(div);
+    }
+    else
+    {
+
+    }
+
+}
