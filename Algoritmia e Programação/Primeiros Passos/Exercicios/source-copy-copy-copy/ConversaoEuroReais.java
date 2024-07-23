afde8369409bcf1f291a9f26c09530e8c99a4de8
JoaoNuno96 <joaonuno1996@hotmail.com>
2024-07-23 21:14:52 +0100

    Pesquisa projecto final Web-Client e Praticando exercicios de Algoritmia

--- Algoritmia e Programação/Primeiros Passos/Exercicios/source-copy-copy-copy/ConversaoEuroReais.java
+++ Algoritmia e Programação/Primeiros Passos/Exercicios/source-copy-copy-copy/ConversaoEuroReais.java
@@ -2,24 +2,16 @@ import java.util.Scanner;
 
 public class ConversaoEuroReais
 {
-    public float euro;
-    public Scanner sc = new Scanner(System.in);
-    
     public ConversaoEuroReais()
-    {
-        this.receberDado();
-        this.conversao();
-    }
-    
-    public void receberDado()
-    {
-        System.out.println("Digite o seu valor em euros: ");
-        euro = sc.nextFloat();
-    }
-    
-    public void conversao()
-    {
-        float resultado = euro * 6.04f;
-        System.out.println("Esse valor em reais é: " + resultado);
+    {   
+        Scanner sc = new Scanner(System.in);
+        System.out.println("CONVERSOR DE EURO PARA REAIS");
+        System.out.println("Apresente o valor (euros):");
+        float numeroRecebido = sc.nextFloat();
+        
+        float valorConvertido = numeroRecebido * 6.04F;
+        
+        System.out.println("O valor de " + numeroRecebido + "€ são " + valorConvertido + " reais.");
+        
     }
 }
